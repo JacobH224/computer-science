fc0f42649595d605f9f8cc6ac3a6e0126be0e4b7
JacobH224 <20jhickey.acc@lwetb.ie>
2024-11-21 12:00:35 +0000

    Add files via upload

--- Questions.py
+++ Questions.py
@@ -8,4 +8,23 @@ print (string)
 #Question 6
 
 string1 = input("Insert a word")
-length = len()
+length = len(string1)
+string2 = string1[::-1]
+if string2 == string1:
+	print ("This is a palindrome")
+else :
+	print ("This is not a palindrome")
+
+#Question 7
+
+#Question 8
+
+string4 = input("Input a word")
+length3 = len(string4)
+print (string4)
+emptystring = ""
+for b in range (0, length3, 2):
+	emptystring += string4[b]
+	if b < (length3-1):
+		emptystring += string4[b+1].upper()
+print (emptystring)
